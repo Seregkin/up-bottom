2c4a0061832c65c1f771037953ca672751f5c6c0
Seregkin <Seregkin@gmail.com>
2016-11-27 16:02:16 +0200

    up bottom

---
+++ bottom.testPlug.js
@@ -0,0 +1,26 @@
+(function($){
+
+	$.fn.testPlug = function(options){ 
+
+		var options = $.extend({ 
+			speed: 800
+		}, options);
+
+
+		var make = function(){
+
+			$(this).click(function(e){
+				e.preventDefault();
+				var id = $(this).attr('href');
+				var offset = $(id).offset().top;
+				$('html, body').animate({
+					scrollTop: offset
+				}, options.speed);
+			});
+
+		};
+
+		return this.each(make) 
+	}
+
+}(jQuery))
